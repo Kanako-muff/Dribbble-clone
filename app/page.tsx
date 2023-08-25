a5f897458803a529551dee0a21d1aec8d4eb8488
Jackie <kong123890@gmail.com>
2023-08-25 16:06:20 -0700

    please work

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
 type SearchParams = {
-  category?: String;
-  endcursor?: String;
+  category?: string;
+  endcursor?: string;
 }
 
 type Props = {
